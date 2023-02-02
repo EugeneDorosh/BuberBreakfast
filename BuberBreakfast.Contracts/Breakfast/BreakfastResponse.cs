aaa05cb583e25bced35cf9beaf9879becf9f448c
EugeneDorosh <evdorow@gmail.com>
2023-02-02 16:54:13 +0200

    initial

---
+++ BuberBreakfast.Contracts/Breakfast/BreakfastResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuberBreakfast.Contracts.Breakfast
+{
+    public record BreakfastResponse(
+        Guid id,
+        string Name,
+        string Description,
+        DateTime StartDateTime,
+        DateTime EmdDateTime,
+        DateTime LastModifiedDateTime,
+        List<string> Savory,
+        List<string> Sweet);
+}
